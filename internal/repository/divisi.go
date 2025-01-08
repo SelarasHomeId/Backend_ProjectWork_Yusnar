edcdfbb405bf904960ddb21769d33b6200898fe9
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2025-01-08 15:43:36 +0700

    fix: refactor query get all divisi

--- internal/repository/divisi.go
+++ internal/repository/divisi.go
@@ -14,6 +14,8 @@ type Divisi interface {
 	Find(ctx *abstraction.Context) (data []*model.DivisiEntityModel, err error)
 	Count(ctx *abstraction.Context) (data *int, err error)
 	Update(ctx *abstraction.Context, data *model.DivisiEntityModel) *gorm.DB
+	FindAll(ctx *abstraction.Context) (data []*model.DivisiEntityModel, err error)
+	CountAll(ctx *abstraction.Context) (data *int, err error)
 }
 
 type divisi struct {
@@ -75,4 +77,31 @@ func (r *divisi) Count(ctx *abstraction.Context) (data *int, err error) {
 
 func (r *divisi) Update(ctx *abstraction.Context, data *model.DivisiEntityModel) *gorm.DB {
 	return r.CheckTrx(ctx).Model(data).Where("id = ?", data.ID).Updates(data)
+}
+
+func (r *divisi) FindAll(ctx *abstraction.Context) (data []*model.DivisiEntityModel, err error) {
+	where, whereParam := general.ProcessWhereParam(ctx, "divisi", "is_delete = @false")
+	limit, offset := general.ProcessLimitOffset(ctx)
+	order := general.ProcessOrder(ctx)
+	err = r.CheckTrx(ctx).
+		Where(where, whereParam).
+		Order(order).
+		Limit(limit).
+		Offset(offset).
+		Find(&data).
+		Error
+	return
+}
+
+func (r *divisi) CountAll(ctx *abstraction.Context) (data *int, err error) {
+	where, whereParam := general.ProcessWhereParam(ctx, "divisi", "is_delete = @false")
+	var count model.DivisiCountDataModel
+	err = r.CheckTrx(ctx).
+		Table("divisi").
+		Select("COUNT(*) AS count").
+		Where(where, whereParam).
+		Find(&count).
+		Error
+	data = &count.Count
+	return
 }
