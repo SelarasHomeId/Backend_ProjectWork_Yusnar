2917f3729080e2dda71f4e6904b6b5731a1cbad0
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2025-01-08 23:38:15 +0700

    fix: rebuild

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"selarashomeid/internal/config"
 	"selarashomeid/pkg/util/validator"
@@ -22,20 +23,20 @@ func Init(e *echo.Echo, redisClient *redis.Client) {
 	e.Use(LoginAttempt(NewLoginAttemptMemoryStore(5)))
 	e.Use(
 		echoMiddleware.Recover(),
-		// echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
-		// 	AllowOrigins: []string{"*"},
-		// 	AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization, echo.HeaderAccessControlAllowOrigin, echo.HeaderAccessControlAllowCredentials, echo.HeaderContentSecurityPolicy, "x-user-id", "ngrok-skip-browser-warning"},
-		// 	AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete, http.MethodPatch},
-		// }),
+		echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
+			AllowOrigins: []string{"*"},
+			AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization, echo.HeaderAccessControlAllowOrigin, echo.HeaderAccessControlAllowCredentials, echo.HeaderContentSecurityPolicy, "x-user-id", "ngrok-skip-browser-warning"},
+			AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete, http.MethodPatch},
+		}),
 		echoMiddleware.LoggerWithConfig(echoMiddleware.LoggerConfig{
 			Format:           fmt.Sprintf("\n| %s | Host: ${host} | Time: ${time_custom} | Status: ${status} | LatencyHuman: ${latency_human} | UserAgent: ${user_agent} | RemoteIp: ${remote_ip} | Method: ${method} | Uri: ${uri} |\n", APP),
 			CustomTimeFormat: "2006/01/02 15:04:05",
 			Output:           os.Stdout,
 		}),
-		// echoMiddleware.SecureWithConfig(echoMiddleware.SecureConfig{
-		// 	XFrameOptions: "DENY",
-		// 	XSSProtection: "1; mode=block",
-		// }),
+		echoMiddleware.SecureWithConfig(echoMiddleware.SecureConfig{
+			XFrameOptions: "DENY",
+			XSSProtection: "1; mode=block",
+		}),
 	)
 	e.HTTPErrorHandler = ErrorHandler
 	e.Validator = &validator.CustomValidator{Validator: validator.NewValidator()}
